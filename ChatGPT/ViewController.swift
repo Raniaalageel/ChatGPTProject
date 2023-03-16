49b1dc6a949cdd9b499f350158f981e2f5ff7de2
Raniaalageel <rania.kh.ala@gmail.com>
2023-03-16 23:59:02 +0300

    Initial Commit

---
+++ ChatGPT/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Rania Alageel on 24/08/1444 AH.
+//
+
+import UIKit
+import AVKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
