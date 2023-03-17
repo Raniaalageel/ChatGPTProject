8b81e91323d84618a1b4d9e75551b10cc2200677
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2023-03-17 21:03:21 +0300

    amani is the best

--- ChatGPT/Engineering/EngModel.swift
+++ ChatGPT/Engineering/EngModel.swift
@@ -15,11 +15,11 @@ struct EngModel{
 extension EngModel {
     static func getMock() -> [EngModel]{
         return  [
-            EngModel(title: "gdsglhf", image: "NBPHE_500x500 (1)", url: "https://stackoverflow.com/questions/28894765/uibutton-action-in-table-view-cell"),
-            EngModel(title: "1jkdlllfjdhl dfahlf  jdhlhf", image: "NBPHE_500x500 (1)", url: ""),
-            EngModel(title: "1jkdfgdfgdgdlllfjdhl dfahlf  jdhlhf", image: "NBPHE_500x500 (1)", url: "String"),
-            EngModel(title: "1jkdfgdfgdflllfjdhl dfahlf  jdhlhf", image: "NBPHE_500x500 (1)", url: ""),
-            EngModel(title: "1jkdfgfdglllfjdhl dfahlf  jdhlhf", image: "NBPHE_500x500 (1)", url: "")
+            EngModel(title: "IASSC Certified Black Belt", image: "5.png", url: "https://iassc.org/six-sigma-certification/black-belt-certification/"),
+            EngModel(title: "Strategic Management Performance System Certification (SMPS)", image: "44", url: "https://www.informa-mea.com/online-programmes/strategy-management/strategic-management-performance-system-smps-certification-online-programmes"),
+            EngModel(title: "Certified Wireless Network Administrator (CWNA)", image: "3", url: "https://www.cwnp.com/certifications/cwna"),
+            EngModel(title: "ARINC Installer, Technician (AFI/AFT)", image: "2", url: "https://www.etai.org/fiber_cabling.html"),
+            EngModel(title: "Leadership in Energy and Environmental Design (LEED)", image: "1", url: "https://www.usgbc.org/leed")
         ]
         
     }
