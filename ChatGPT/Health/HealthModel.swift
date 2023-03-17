ccf6f56c849ecc1cdede2b780075f74c1fd5a587
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2023-03-17 19:15:38 +0300

    ،،

--- ChatGPT/Health/HealthModel.swift
+++ ChatGPT/Health/HealthModel.swift
@@ -20,7 +20,7 @@ extension HealthModel {
             HealthModel(title: "Certified Healthcare Facility Manager (CHFM)", image: "NBPHE_500x500 (1)", url: ""),
             HealthModel(title: "Certified in Public Health (CPH)", image: "NBPHE_500x500 (1)", url: "String"),
             HealthModel(title: "Medical Technologist (MT)", image: "mt_badge_logo_medical_technologist_laboratory_classic_round_sticker-ra924e3e114b94c39873b47b6d4e712a2_0ugmp_8byvr_736-removebg-preview", url: ""),
-            HealthModel(title: "Certified Clinical Research Associate(CCRA)", image: "NBPHE_500x500 (1)", url: "")
+            HealthModel(title: "Certified Clinical Research Associate(CCRA)", image: "CCRA", url: "647927bc-b2bd-49eb-a2db-8424fdee66df-151215125404-thumbnail-removebg-preview")
         ]
         
     }
