9395958aae692bc9860095825d452dfbb13ed363
MsAlshamikh <98359943+MsAlshamikh@users.noreply.github.com>
2023-03-17 20:42:45 +0300

    2

--- ChatGPT/Bus/BusModel.swift
+++ ChatGPT/Bus/BusModel.swift
@@ -16,8 +16,8 @@ extension BusModel {
     static func getMock() -> [BusModel]{
         return  [
             BusModel(title: "Certified Fraud Examiner (CFE)", image: "cfe", url: "https://stackoverflow.com/questions/28894765/uibutton-action-in-table-view-cell"),
-            BusModel(title: "Financial Modeling & Valuation Analyst FMVA", image: "FMVA", url: ""),
-            BusModel(title: "Talent Management Certification Program", image: "tmcp1", url: "String"),
+            BusModel(title: "Financial Modeling & Valuation Analyst FMVA", image: "FMVA", url: "https://corporatefinanceinstitute.com/certifications/financial-modeling-valuation-analyst-fmva-program/"),
+            BusModel(title: "Talent Management Certification Program", image: "tmcp1", url: "https://instituteod.com/organization-development-certification-programs/talent-management-certified-professional/"),
             BusModel(title: "Brand Management Certificate Program", image: "Cornell_University_Logo", url: ""),
             BusModel(title: "Certified Associate in Project Management (CAPM)", image: "capm-600px", url: "https://www.rics.org/training-events/online-training/on-demand/certified-associate-project-management?&cid=PPC%7cGoogle%7cTrainingEventsDSAMENA%7cLowValue%7cDSA%7cAll&gclid=Cj0KCQjwn9CgBhDjARIsAD15h0C6ZZY0-DPecE-ns54YbFwInHa5ylyTcJduGaataix02JwXeAaEesgaAn-CEALw_wcB&gclsrc=aw.ds")
         ]
