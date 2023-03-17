80494958ab2e8365789ccfb952d4194c8839a694
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2023-03-17 15:14:30 +0300

    9m

---
+++ ChatGPT/Health.swift
@@ -0,0 +1,29 @@
+//
+//  Health.swift
+//  ChatGPT
+//
+//  Created by Amani Aldahmash on 17/03/2023.
+//
+
+import UIKit
+
+class Health: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
