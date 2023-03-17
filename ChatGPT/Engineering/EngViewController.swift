f81c93eef030199017cda118ec129dd604698367
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2023-03-17 20:17:06 +0300

    amanigameover

---
+++ ChatGPT/Engineering/EngViewController.swift
@@ -0,0 +1,67 @@
+//
+//  EngViewController.swift
+//  ChatGPT
+//
+//  Created by Amani Aldahmash on 17/03/2023.
+//
+
+import UIKit
+
+class EngViewController: UIViewController, UITableViewDelegate ,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+ let EngModelList: [EngModel] = EngModel.getMock()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupUI(){
+        let cellNib = UINib(nibName: "HealthTableViewCell", bundle: Bundle.main)
+        tableView.register(cellNib, forCellReuseIdentifier: "healthTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  EngModelList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "healthTableViewCell", for: indexPath) as! HealthTableViewCell
+        let  busModel  =  EngModelList[indexPath.row]
+        var healthModel = HealthModel(title: busModel.title, image: busModel.image, url: busModel.url)
+        cell.setupUI(healthModel)
+        cell.loacationButton.addTarget(self, action: #selector(connected(sender:)), for: .touchUpInside)
+        //  id =1
+        cell.loacationButton.tag = indexPath.row
+
+        return cell
+    }
+    
+    
+    @objc func connected(sender: UIButton){
+        let buttonTag = sender.tag
+        UIApplication.shared.open(URL(string: EngModelList[buttonTag].url)! as  URL, options:  [:], completionHandler: nil)
+        // open web healthList [sender.tag]
+    
+    }
+}
